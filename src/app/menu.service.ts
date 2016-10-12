15ba6dd83a31b0c605dfca158288c386065b4531
Ozan Müyesseroğlu <ozanmuyes@gmail.com>
2016-10-12 23:44:37 +0300

    Got RxJS working

--- src/app/menu.service.ts
+++ src/app/menu.service.ts
@@ -1,8 +1,10 @@
-import {Http} from "@angular/http";
+import {Http, Response} from "@angular/http";
 
 import {Item} from "./item";
-import {Observable} from "rxjs";
+import {Observable} from "rxjs/Observable";
+import {Injectable} from "@angular/core";
 
+@Injectable()
 export class MenuService {
   private url: string = 'http://localhost:3000/users';
 
@@ -11,18 +13,16 @@ export class MenuService {
 
   getMenuItems(): Observable<Item[]> {
     return this.http.get(this.url)
-      .map((data) => data.json());
+      .map((data) => data.json())
+      .catch(this.handleError);
   }
 
-  // getMenuItems(): Promise<Item[]> {
-  //   return this.http
-  //     .get(this.url)
-  //     .toPromise()
-  //     .then(res => res.json() as Item[])
-  //     .catch(this.handleError);
-  // }
-
-  private handleError() {
-
+  private handleError(error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
   }
 }
